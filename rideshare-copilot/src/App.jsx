531f84fc6796dcd782a9b0b54fd143ebe327bccc
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-09 17:04:30 +0300

    RC-24 Wrap the entire app in a query client provider and activate dev tools

---
+++ rideshare-copilot/src/App.jsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
