2fdeea5934037a5c1d14901bd041c91b53b96f96
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-10 16:09:32 +0300

    Define a logo component

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,5 @@
+function Logo() {
+  return <h1>Rideshare Copilot Logo</h1>;
+}
+
+export default Logo;
--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -2,12 +2,14 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 import Nav from "./Nav";
+import Logo from "./Logo";
 
 const StyledNavBar = styled.nav``;
 
 function NavBar() {
   return (
     <StyledNavBar>
+      <Logo />
       <Nav />
     </StyledNavBar>
   );
