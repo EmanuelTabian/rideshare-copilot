868376679fe069ca3f4ded757762a2d7e74d52a6
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-11 18:56:18 +0300

    Restructure car card component

--- src/ui/CarCard.jsx
+++ src/ui/CarCard.jsx
@@ -1,21 +1,58 @@
+import { version } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledListItem = styled.li``;
-
+const CarInfo = styled.div``;
+const CarHeader = styled.div``;
+const CarHeaderData = styled.span``;
+const CarDetailsData = styled.span``;
+const CarDetails = styled.span``;
+const AdditionalInfo = styled.div``;
+const Img = styled.img``;
+const Button = styled.button``;
 function CarCard({ carDetails }) {
   const navigate = useNavigate();
 
-  const { name, model, image, year, id, createdAt } = carDetails;
+  const {
+    name,
+    model,
+    image,
+    year,
+    version,
+    engine,
+    power,
+    milleage,
+    fuelType,
+    color,
+    createdAt,
+    location,
+    id,
+  } = carDetails;
   return (
     <StyledListItem>
-      <span>{createdAt}</span>
-      <img src={image} alt={name} />
-      <span> {name}</span>
-      <span> {model}</span>
-      <span> {year}</span>
-      <span> {name}</span>
-      <button onClick={() => navigate(`/cars/${id}`)}>See details</button>
+      <Img src={image} alt={name} />
+      <CarInfo>
+        <CarHeader>
+          <CarHeaderData> {name}</CarHeaderData>
+          <CarHeaderData> {model}</CarHeaderData>
+          <CarHeaderData> {version}</CarHeaderData>
+          <CarHeaderData> {engine}</CarHeaderData>
+          <CarHeaderData> {version}</CarHeaderData>
+          <CarHeaderData> {power} CP</CarHeaderData>
+          <CarHeaderData> {color}</CarHeaderData>
+        </CarHeader>
+        <CarDetails>
+          <CarDetailsData> {milleage} km</CarDetailsData>
+          <CarDetailsData> {fuelType}</CarDetailsData>
+          <CarDetailsData> {year}</CarDetailsData>
+        </CarDetails>
+        <AdditionalInfo>
+          <CarDetailsData> {createdAt}</CarDetailsData>
+          <CarDetailsData> {location}</CarDetailsData>
+        </AdditionalInfo>
+      </CarInfo>
+      <Button onClick={() => navigate(`/cars/${id}`)}>See details</Button>
     </StyledListItem>
   );
 }
