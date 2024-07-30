7f3243ded46bf4fc158af83fe4adb662a62832cd
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 13:04:27 +0300

    RC-27 Setup a nav link for the new route

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { cars } from "../data/cars-data";
 import CarCard from "../ui/CarCard";
+import { NavLink } from "react-router-dom";
 
 const StyledCars = styled.div``;
 
@@ -11,14 +12,17 @@ const CarList = styled.ul``;
 
 function Cars() {
   return (
-    <StyledCars>
-      <H2>Browse car posts</H2>
-      <CarList>
-        {cars.map((car) => (
-          <CarCard key={car.id} carDetails={car} />
-        ))}
-      </CarList>
-    </StyledCars>
+    <>
+      <StyledCars>
+        <H2>Browse car posts</H2>
+        <CarList>
+          {cars.map((car) => (
+            <CarCard key={car.id} carDetails={car} />
+          ))}
+        </CarList>
+      </StyledCars>
+      <NavLink to="/cars/myposts">My posts</NavLink>
+    </>
   );
 }
 
