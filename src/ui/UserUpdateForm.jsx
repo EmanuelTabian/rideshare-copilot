1fb3089334b624f23ef2f73558ee57f988d56d97
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 18:53:23 +0300

    RC-27 Define user update info component structure

--- src/ui/UserUpdateForm.jsx
+++ src/ui/UserUpdateForm.jsx
@@ -1,5 +1,25 @@
+import { useForm } from "react-hook-form";
+import Button from "./Button";
+
 function UserUpdateForm() {
-  return <div></div>;
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+  return (
+    <form>
+      <fieldset>
+        <legend>Update profile info</legend>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input type="text" {...register("username")} />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input type="password" {...register("password")} />
+        </div>
+        <Button>Save</Button>
+      </fieldset>
+    </form>
+  );
 }
 
 export default UserUpdateForm;
