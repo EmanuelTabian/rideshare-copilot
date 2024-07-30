91fd4ddce43c9fdb00609d84e9b244bec8da50e3
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 18:40:09 +0300

    RC-27 Buld a dummy document card component

--- src/pages/Documents.jsx
+++ src/pages/Documents.jsx
@@ -1,6 +1,7 @@
 import Button from "../ui/Button";
 import Message from "../ui/Message";
 import DocumentUploadForm from "../ui/DocumentUploadForm";
+import DocumentCard from "../ui/DocumentCard";
 
 function Documents() {
   return (
@@ -9,6 +10,7 @@ function Documents() {
       {/* Conditionally rendered when the documents array of objects is empty */}
       {/* <Message>No documents uploaded</Message> */}
       <Button>Add new document</Button>
+      <DocumentCard />
       <DocumentUploadForm />
     </>
   );
