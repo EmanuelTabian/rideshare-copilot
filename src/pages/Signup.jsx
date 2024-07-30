c96c3ad00a92400c94e59f4b1e4b5beb190657bf
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 16:09:00 +0300

    RC-27 Build the signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,47 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
+
 function Signup() {
-  return <div>Signup</div>;
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+  const navigate = useNavigate();
+
+  function onSubmit() {
+    navigate("/dashboard");
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <legend>Enter your email and password</legend>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            {...register("email", { required: "This field is required" })}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            {...register("username", { required: "This field is required" })}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            {...register("password", { required: "This field is required" })}
+          />
+        </div>
+        <div>
+          <Button>Create Account</Button>
+        </div>
+      </fieldset>
+    </form>
+  );
 }
 
 export default Signup;
