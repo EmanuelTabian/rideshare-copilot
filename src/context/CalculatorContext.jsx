89cf0d97b2c8d3c7335da342f3572e9841f035e3
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 19:01:58 +0300

    Define the app commission calculator context

---
+++ src/context/CalculatorContext.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext } from "react";
+import { useState } from "react";
+const CalcContext = createContext();
+
+function UseCalculatorContext({ children }) {
+  const [income, setIncome] = useState("");
+  const [rideCom, setRideCom] = useState("");
+  const [emplCom, setEmplCom] = useState("");
+  const [otherCom, setOtherCom] = useState("");
+  const [gasExp, setGasExp] = useState("");
+  const [mealsExp, setMealsExp] = useState("");
+  const [otherExp, setOtherExp] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const commissionPerc = rideCom + emplCom;
+  const totalExpenses = Number(gasExp) + Number(mealsExp) + Number(otherExp);
+  const netIncome = Math.round(
+    income - (commissionPerc * income) / 100 - otherCom - totalExpenses
+  );
+
+  return (
+    <CalcContext.Provider
+      value={{
+        income,
+        setIncome,
+        rideCom,
+        setRideCom,
+        emplCom,
+        setEmplCom,
+        otherCom,
+        setOtherCom,
+        gasExp,
+        setGasExp,
+        mealsExp,
+        setMealsExp,
+        otherExp,
+        setOtherExp,
+        toggle,
+        setToggle,
+        commissionPerc,
+        totalExpenses,
+        netIncome,
+      }}
+    >
+      {children}
+    </CalcContext.Provider>
+  );
+}
+
+function UseCalculator() {
+  const context = useContext(CalcContext);
+  if (context === undefined)
+    throw new Error("Calculator context used outside CalcContext provider!");
+  return context;
+}
+
+export { UseCalculatorContext, UseCalculator };
