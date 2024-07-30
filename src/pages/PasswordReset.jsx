6562fcf646128fd5c5fe983f6ce15679c69588b4
EmanuelTabian <emanuel.forwork@gmail.com>
2024-07-30 16:22:29 +0300

    RC-27 Build the password reset form

--- src/pages/PasswordReset.jsx
+++ src/pages/PasswordReset.jsx
@@ -1,5 +1,29 @@
+import { useForm } from "react-hook-form";
+import Button from "../ui/Button";
+
 function PasswordReset() {
-  return <div>Password reset</div>;
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+  return (
+    <>
+      <h1>Reset Your Password</h1>
+      <form>
+        <fieldset>
+          <legend>
+            Enter your email address and we'll ed you a password reset link.
+          </legend>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              {...register("email", { required: "This field is required" })}
+            />
+          </div>
+          <Button>Reset password </Button>
+        </fieldset>
+      </form>
+    </>
+  );
 }
 
 export default PasswordReset;
