b694c808c8338ffcdc6009793e98d423c6f9307c
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-02 15:45:37 +0300

    RC-56 Define a signup service

--- src/services/rideauthApi.js
+++ src/services/rideauthApi.js
@@ -1,1 +1,10 @@
 const ridebackendURL = "http://127.0.0.1:8000/api";
+
+export async function signup(userdata) {
+  try {
+    const response = await axios.post(`${ridebackendURL}/register`, userdata);
+    return response.data;
+  } catch (err) {
+    throw new Error(`${err.message} You were unable to sign up! ☹️`);
+  }
+}
