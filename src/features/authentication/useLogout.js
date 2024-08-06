fc8fc9b8a6df6ca58856723be909ca1eb9dcd8b3
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-06 18:44:48 +0300

    RC-56 Handle the button signout click event and and use navigate hook

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../services/rideauthApi";
+import { useNavigate } from "react-router-dom";
 
-function useLogout() {
+export function useLogout() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient;
   const { mutate: logout, isLoading } = useMutation({
     mutationFn: logoutApi,
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,17 +3,20 @@ import styled from "styled-components";
 import { IoSettings } from "react-icons/io5";
 import { IoLogOutOutline } from "react-icons/io5";
 import Button from "./Button";
+import { useLogout } from "../features/authentication/useLogout";
 
 const StyledHeaderMenu = styled.div``;
 
 function HeaderMenu() {
+  const { logout, isLoadin } = useLogout();
+
   return (
     <StyledHeaderMenu>
       <NavLink to="/settings">
         <IoSettings />
       </NavLink>
       <Button>
-        <IoLogOutOutline />
+        <IoLogOutOutline onClick={logout} />
       </Button>
     </StyledHeaderMenu>
   );
