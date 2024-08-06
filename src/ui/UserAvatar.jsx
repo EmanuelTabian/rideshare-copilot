2232bcbae50612a125f708b8c02f4d2c8ae8a793
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-06 18:10:10 +0300

    RC-56 Render the user name on the app header

--- src/ui/UserAvatar.jsx
+++ src/ui/UserAvatar.jsx
@@ -1,18 +1,21 @@
 import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
 
 const StyledUserAvatar = styled.div``;
 const Avatar = styled.img``;
 
 function UserAvatar() {
+  const { user } = useUser();
+
   const avatar = "";
   const username = "";
   return (
     <StyledUserAvatar>
       <Avatar
         src={avatar || "default-user.jpg"}
-        alt={`Avatar of ${username}`}
+        alt={`Avatar of ${user.name}`}
       />
-      <span>%Username%</span>
+      <h1>Hi {user.name}</h1>
     </StyledUserAvatar>
   );
 }
