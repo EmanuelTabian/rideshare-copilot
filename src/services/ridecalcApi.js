cff17f25331f5ed4892b6fffbc06850fb51a4e6b
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-22 13:35:44 +0300

    RC-74 Write an async function that gets all the calculator entries associated to the logged in user

--- src/services/ridecalcApi.js
+++ src/services/ridecalcApi.js
@@ -2,3 +2,16 @@ import axios from "axios";
 import { ridebackendURL } from "./rideauthApi";
 
 axios.defaults.withCredentials = true;
+
+export async function getCalculatorEntries() {
+  try {
+    const response = await axios.get(
+      `${ridebackendURL}/get-calculator-entries`
+    );
+    return response.data;
+  } catch (err) {
+    throw new Error(
+      `${err.message} Sorry, we were unable to retrieve your calculator entries! ☹️`
+    );
+  }
+}
