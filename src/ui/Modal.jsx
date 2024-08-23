9e9debf07f6fb3e757ffff575f5f9e3f5297c27e
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-23 13:30:38 +0300

    RC-77Build the window subcomponent that creates a portal in order to avoid capturing and bubbling issues

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,6 @@
-import { cloneElement, createContext, useState } from "react";
+import { cloneElement, createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 // Modal compound component originally designed by Jonas Schmedtmann.
 
@@ -22,5 +24,26 @@ function Open({ children, opens: opensWindowName }) {
   // We are not able to attatch the handler directly on the button so we're cloning the element and attatch it here
   return cloneElement(children, { onClick: () => open(opensWindowName) });
 }
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useCloseForm(close);
+
+  if (name !== openName) return null;
+  // Create a portel to append the element directly to the body in order to avoid event bubbling and capturing issues
+  return createPortal(
+    <div>
+      <div ref={ref}>
+        <button onClick={close}>
+          <IoCloseCircleOutline />
+        </button>
+        <div>{cloneElement(children, { onCLoseModal: close })}</div>
+      </div>
+    </div>
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
