08be6b0ac6e1161ebde11388d48da31862d25768
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-23 13:00:43 +0300

    RC-77 Define a modal compound component that handles both edit form and deletion confirmation pop-up opening and closing

---
+++ src/ui/Modal.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+// Modal compound component originally designed by Jonas Schmedtmann.
+
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export default Modal;
