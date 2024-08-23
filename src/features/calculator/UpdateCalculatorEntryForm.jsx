1298185d7ccb94ce5e90b8de212ef316b82f2250
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-23 15:45:46 +0300

    RC-77 Render calculator entry form and the confirmation prompt

--- src/features/calculator/UpdateCalculatorEntryForm.jsx
+++ src/features/calculator/UpdateCalculatorEntryForm.jsx
@@ -1,4 +1,9 @@
-function UpdateCalculatorEntryForm() {
+import { useForm } from "react-hook-form";
+
+function UpdateCalculatorEntryForm({ calculatorEntry, onClose }) {
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+
   return (
     <form>
       <fieldset>
@@ -12,7 +17,7 @@ function UpdateCalculatorEntryForm() {
         <input type="number" {...register("commission")} />
         <label htmlFor="expenses">Expenses:</label>
         <input type="number" {...register("expenses")} />
-        <label htmlFor="earnings">App income:</label>
+        <label htmlFor="earnings">Earnings</label>
         {/* This field will compute the new values and perform a new calculation for the expenses */}
         <input disabled={true} type="number" {...register("earnings")} />
       </fieldset>
