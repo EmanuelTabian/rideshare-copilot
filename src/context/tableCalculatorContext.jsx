63590e07906ffe209e0c632e0d78b038e962deea
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-24 16:47:15 +0300

    RC-77 Define a reducer that handles table calculator state changes

--- src/context/tableCalculatorContext.jsx
+++ src/context/tableCalculatorContext.jsx
@@ -1,10 +1,61 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+
+const InitialState = {
+  income: "",
+  commission: "",
+  expenses: "",
+  earnings: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setIncome":
+      return {
+        ...state,
+        income: action.payload,
+        earnings: Math.round(
+          state.income -
+            (state.commission * state.income) / 100 -
+            state.expenses
+        ),
+      };
+    case "setCommission":
+      return {
+        ...state,
+        commission: action.payload,
+        earnings: Math.round(
+          state.income -
+            (state.commission * state.income) / 100 -
+            state.expenses
+        ),
+      };
+    case "setExpenses":
+      return {
+        ...state,
+        expenses: action.payload,
+        earnings: Math.round(
+          state.income -
+            (state.commission * state.income) / 100 -
+            state.expenses
+        ),
+      };
+    default:
+      throw new Error("Action unrecognized!");
+  }
+}
 
 const TableCalculatorContext = createContext();
 
 function tableCalculatorProvider({ children }) {
+  const [{ income, commission, expenses, earnings }, dispatch] = useReducer(
+    reducer,
+    InitialState
+  );
+
   return (
-    <TableCalculatorContext.Provider value={{}}>
+    <TableCalculatorContext.Provider
+      value={{ income, commission, expenses, earnings, dispatch }}
+    >
       {children}
     </TableCalculatorContext.Provider>
   );
