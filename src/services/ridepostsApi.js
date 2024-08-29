d15ef41a749a9af2443cb2092e5230b9b3fa77ee
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-29 17:45:34 +0300

    RC-70 Start the uploading process

--- src/services/ridepostsApi.js
+++ src/services/ridepostsApi.js
@@ -6,16 +6,23 @@ axios.defaults.withCredentials = true;
 export async function directUploadStart(data) {
   const fileName = data.image[0].name;
   const fileType = `image/${data.image[0].name.slice(-3)}`;
-  // const fileName = data.image[0].name.slice(0, image.lastIndexOf("."));
-
-  console.log(fileType, fileName);
+  const file = data.image[0];
 
   try {
+    // Get the presigned data
     const response = await axios.post(`${ridebackendURL}/upload/direct/start`, {
       file_name: fileName,
       file_type: fileType,
     });
-    console.log(response);
+    console.log(response.data);
+    const { url, fields } = response.data;
+    const postData = new FormData();
+    Object.entries(fields).forEach(([key, value]) =>
+      postData.append(key, value)
+    );
+    postData.append("file", file);
+    const uploadData = await axios.post(url, postData);
+    console.log(uploadData);
   } catch (err) {
     throw new Error(
       `${err.message} Sorry, we were unable to start the upload process! ☹️`
