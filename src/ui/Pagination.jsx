c4db12370feba0f05cca8ae84958cdaf22e80684
EmanuelTabian <emanuel.forwork@gmail.com>
2024-09-08 18:54:28 +0300

    RC-87 Set up a dummy pagination component for page param set test purpose

---
+++ src/ui/Pagination.jsx
@@ -0,0 +1,42 @@
+import { useSearchParams } from "react-router-dom";
+import { HiChevronDoubleLeft } from "react-icons/hi2";
+import { HiChevronDoubleRight } from "react-icons/hi2";
+import { HiChevronLeft } from "react-icons/hi2";
+import { HiChevronRight } from "react-icons/hi2";
+
+function Pagination({ count }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentPage = !searchParams.get(page)
+    ? 1
+    : Number(searchParams.get("page"));
+
+  // On a future refactor the value 10 will be modified to a specific value selected by the user that will also be sent to backend for specific post number fetch
+  const pageCount = Math.ceil(count / 10);
+
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
+    searchParams.set("page", next);
+    setSearchParams(searchParams);
+  }
+
+  function previousPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+    searchParams("page", prev);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <div>
+      <button onClick={previousPage} disabled={currentPage === 1}>
+        <HiChevronLeft /> <span>Previous</span>
+      </button>
+      <button onClick={nextPage} disabled={currentPage === pageCount}>
+        <span>Next</span>
+        <HiChevronRight />
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
