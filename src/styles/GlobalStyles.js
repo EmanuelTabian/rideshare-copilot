99a0e97979ecf605dca967046137658ccf974108
EmanuelTabian <emanuel.forwork@gmail.com>
2024-09-26 16:44:39 +0300

    RC-30 Set a page title, a new font and the background color

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,5 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyles = createGlobalStyle``;
+const GlobalStyles = createGlobalStyle`
+:root {
+    --app-background-color: linear-gradient(to bottom, #BEFFD9, #ffffff);
+}
+
+
+ * {
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+ }
+
+ body {
+    background: var(--app-background-color) ;
+ }
+`;
 
 export default GlobalStyles;
