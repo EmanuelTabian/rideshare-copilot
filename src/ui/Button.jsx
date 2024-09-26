74693ad40ad449537632c334216f5b59e30e3446
EmanuelTabian <emanuel.forwork@gmail.com>
2024-09-26 17:28:20 +0300

    RC-30 Stylize the button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: var(--color-brand-600);
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  width: 100%;
+  max-width: 300px;
+  margin: 16px auto;
+  display: block;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+`;
+
 function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 }
 
 export default Button;
