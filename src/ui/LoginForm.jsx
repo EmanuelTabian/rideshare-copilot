37a866625de44f0365d7266d6b789ec549c1a3c9
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-03 15:09:04 +0300

    RC-103 Set up login form background to white so it matches the other containers

--- src/ui/LoginForm.jsx
+++ src/ui/LoginForm.jsx
@@ -45,7 +45,7 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
   padding: 1rem;
-  background-color: #f9f9f9;
+  background-color: white;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 
