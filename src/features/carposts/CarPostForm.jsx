0e5a3566316913e82474fc715accab8e389b40eb
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-06 19:27:43 +0300

    RC-101 Organize form input in three fieldsets that match specs categories

--- src/features/carposts/CarPostForm.jsx
+++ src/features/carposts/CarPostForm.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useAddCarPost } from "./useAddCarPost";
 import { useUpdateCarPost } from "./useUpdateCarPost";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledContainer = styled.div`
   padding: 16px;
@@ -86,182 +87,200 @@ function Form({ carDetails = {}, onCloseModal }) {
   return (
     <StyledContainer>
       <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <fieldset>
-          <legend>Add a car rental post</legend>
-          <div>
-            <label htmlFor="car_name">Name</label>
-            <input
-              type="text"
-              id="car_name"
-              {...register("car_name", { required: "This field is required" })}
-            />
-          </div>
-          <div>
-            <label htmlFor="model">Model</label>
-            <input
-              type="text"
-              id="model"
-              {...register("model", { required: "This field is required" })}
-            />
-          </div>
-          <div>
-            <label htmlFor="version">Version</label>
-            <input type="text" id="version" {...register("version")} />
-          </div>
-          <div>
-            <label htmlFor="year">Year</label>
-            <input
-              type="text"
-              id="year"
-              {...register("year", { required: "This field is required" })}
-            />
-          </div>
-          <div>
-            <label htmlFor="engine">Engine</label>
-            <input
-              type="text"
-              id="engine"
-              {...register("engine", { required: "This field is required" })}
-            />
-          </div>
-          <div>
-            <label htmlFor="fuel">Fuel</label>
-            <input
-              type="text"
-              id="fuel"
-              {...register("fuel", { required: "This field is required" })}
-            />
-          </div>{" "}
-          <div>
-            <label htmlFor="body">Body</label>
-            <input
-              type="text"
-              id="body"
-              {...register("body", { required: "This field is required" })}
-            />
-          </div>
-          <div>
-            <label htmlFor="transmission">Transmission</label>
-            <input
-              type="text"
-              id="transmission"
-              {...register("transmission", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="gear_number">Gears</label>
-            <input
-              type="number"
-              id="gear_number"
-              {...register("gear_number", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="color">Color</label>
-            <input
-              type="text"
-              id="color"
-              {...register("color", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="seat_number">Seats</label>
-            <input
-              type="number"
-              id="seat_number"
-              {...register("seat_number", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="door_number">Doors</label>
-            <input
-              type="number"
-              id="door_number"
-              {...register("door_number")}
-            />
-          </div>
-          <div>
-            <label htmlFor="milleage">Milleage</label>
-            <input
-              type="number"
-              id="milleage"
-              {...register("milleage", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="power">Power</label>
-            <input
-              type="number"
-              id="power"
-              {...register("power", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="mpg">Mpg</label>
-            <input type="number" id="mpg" {...register("mpg")} />
-          </div>
-          <div>
-            <label htmlFor="description">Description</label>
-            <textarea
-              type="text"
-              id="description"
-              {...register("description")}
-            />
-          </div>
-          <div>
-            <label htmlFor="location">Location</label>
-            <input type="text" id="location" {...register("location")} />
-          </div>
-          <div>
-            <label htmlFor="emission_standard">Emmision</label>
-            <input
-              type="text"
-              id="emission_standard"
-              {...register("emission_standard", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="price">Price</label>
-            <input
-              type="number"
-              id="price"
-              {...register("price", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="contact">Contact</label>
-            <input
-              type="text"
-              id="contact"
-              {...register("contact", {
-                required: "This field is required",
-              })}
-            />
-          </div>
-          <div>
-            <label htmlFor="image">Image</label>
-            <input type="file" id="image" {...register("image")} />
-          </div>
-          <div>
-            <input type="submit" />
-          </div>
-        </fieldset>
+        {formSession === 1 && (
+          <fieldset>
+            <legend>Basic Car Specs</legend>
+            <div>
+              <label htmlFor="car_name">Name</label>
+              <input
+                type="text"
+                id="car_name"
+                {...register("car_name", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="model">Model</label>
+              <input
+                type="text"
+                id="model"
+                {...register("model", { required: "This field is required" })}
+              />
+            </div>
+            <div>
+              <label htmlFor="year">Year</label>
+              <input
+                type="text"
+                id="year"
+                {...register("year", { required: "This field is required" })}
+              />
+            </div>
+            <div>
+              <label htmlFor="engine">Engine</label>
+              <input
+                type="text"
+                id="engine"
+                {...register("engine", { required: "This field is required" })}
+              />
+            </div>
+            <div>
+              <label htmlFor="fuel">Fuel</label>
+              <input
+                type="text"
+                id="fuel"
+                {...register("fuel", { required: "This field is required" })}
+              />
+            </div>{" "}
+            <div>
+              <label htmlFor="power">Power</label>
+              <input
+                type="number"
+                id="power"
+                {...register("power", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="milleage">Milleage</label>
+              <input
+                type="number"
+                id="milleage"
+                {...register("milleage", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="contact">Contact</label>
+              <input
+                type="text"
+                id="contact"
+                {...register("contact", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="price">Price</label>
+              <input
+                type="number"
+                id="price"
+                {...register("price", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+          </fieldset>
+        )}
+        {formSession === 2 && (
+          <fieldset>
+            <legend>Additional Car Specs</legend>
+            <div>
+              <label htmlFor="color">Color</label>
+              <input
+                type="text"
+                id="color"
+                {...register("color", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="body">Body</label>
+              <input
+                type="text"
+                id="body"
+                {...register("body", { required: "This field is required" })}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="transmission">Transmission</label>
+              <input
+                type="text"
+                id="transmission"
+                {...register("transmission", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="gear_number">Gears</label>
+              <input
+                type="number"
+                id="gear_number"
+                {...register("gear_number", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="emission_standard">Emission</label>
+              <input
+                type="text"
+                id="emission_standard"
+                {...register("emission_standard", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="description">Description</label>
+              <textarea
+                type="text"
+                id="description"
+                {...register("description")}
+              />
+            </div>
+          </fieldset>
+        )}
+        {formSession === 3 && (
+          <fieldset>
+            <legend>Optional Car Specs</legend>
+            <div>
+              <label htmlFor="version">Version</label>
+              <input type="text" id="version" {...register("version")} />
+            </div>
+            <div>
+              <label htmlFor="door_number">Doors</label>
+              <input
+                type="number"
+                id="door_number"
+                {...register("door_number")}
+              />
+            </div>
+            <div>
+              <label htmlFor="seat_number">Seats</label>
+              <input
+                type="number"
+                id="seat_number"
+                {...register("seat_number", {
+                  required: "This field is required",
+                })}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="mpg">Mpg</label>
+              <input type="number" id="mpg" {...register("mpg")} />
+            </div>
+
+            <div>
+              <label htmlFor="location">Location</label>
+              <input type="text" id="location" {...register("location")} />
+            </div>
+
+            <div>
+              <label htmlFor="image">Image</label>
+              <input type="file" id="image" {...register("image")} />
+            </div>
+            <div>
+              <input type="submit" />
+            </div>
+          </fieldset>
+        )}
       </StyledForm>
     </StyledContainer>
   );
